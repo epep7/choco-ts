17fee659b37ea8a2c1c9ccc70f5dca850a6663f6
Ep <epibn60@gmail.com>
2024-11-13 04:38:16 +0700

    added export on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Client } from "./client.ts";
 
+export * from "./auth/auth.ts"
+export * from "./query/query-builder.ts"
+export * from "./error/error-response.ts";
+
 export function createChocoClient(apiKey: string, url: string) {
   return new Client(apiKey, url)
 }
